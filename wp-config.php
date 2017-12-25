d8568b9fbfac93b76a9a882c7e99be073413d5da
zzq <464164213@qq.com>
2017-12-25 09:57:50 +0800

    提交数据库

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress2');
 define('DB_USER', 'root');
 
 /** MySQL数据库密码 */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL主机 */
 define('DB_HOST', 'localhost');
